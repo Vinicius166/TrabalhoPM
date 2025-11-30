01485526c468611e81d346d0bc1e5fea6aa67b94
Vinicius Duarte <vinicius-duarte2013@aluno.ufsj.edu.br>
2025-11-30 14:40:52 -0300

    Add run instructions for sistemaViagens
    Added instructions to run the program from the parent directory.

--- sistema/sistema/sistemaViagens/run.py
+++ sistema/sistema/sistemaViagens/run.py
@@ -1,5 +1,7 @@
-
 import sistemaViagens.viagens.programa as prog
+
+# comando para rodar o programa estando no diretorio sistema anterior ao sistemaViagens:
+# python3 -m sistemaViagens.run
 
 if __name__ == '__main__':
     prog.main()
