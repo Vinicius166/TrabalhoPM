bf34d1939ccd5d5bb1de1721cea8eea19e0abafb
Vinicius Duarte <vinicius-duarte2013@aluno.ufsj.edu.br>
2025-12-07 23:16:46 -0300

    Update initial destinations and clients in programa.py

--- sistema/sistema/sistemaViagens/viagens/programa.py
+++ sistema/sistema/sistemaViagens/viagens/programa.py
@@ -7,20 +7,20 @@ def main():
     
     
     destinos_iniciais = [
-        Destino(1, "Itacaré", "Barracuda Boutique", 185.00),
-        Destino(25244, "Salvador", "Fera Palace Hotel", 150.00),
-        Destino(39782, "Porto Seguro", "La Torre Resort", 130.00),
-        Destino(45497, "Arraial d'Ajuda", "Pousada Maria Pitanga", 110.00),
-        Destino(55484, "Maraú", "Brisa da Barra Suítes", 175.00),
-        Destino(65484, "Valença", "Hotel Galeão", 95.00)
+        Destino(00000, "Itacaré", "Barracuda Boutique", 185.00),
+        Destino(11111, "Salvador", "Fera Palace Hotel", 150.00),
+        Destino(22222, "Porto Seguro", "La Torre Resort", 130.00),
+        Destino(33333, "Arraial d'Ajuda", "Pousada Maria Pitanga", 110.00),
+        Destino(44444, "Maraú", "Brisa da Barra Suítes", 175.00),
+        Destino(55555, "Valença", "Hotel Galeão", 95.00)
     ]
    
     for d in destinos_iniciais:
         banco.destinos.inserir(d)
     #teste abaixo
     cliente_inicial = [
-        Cliente(10, 'Vinicius', 'vinicius@email.com'),
-        Cliente(11, 'Matheus', 'matheus@email.com')
+        Cliente(1, 'Vinicius', 'vinicius@email.com'),
+        Cliente(2, 'Matheus', 'matheus@email.com')
     ]
 
     for d in cliente_inicial:
